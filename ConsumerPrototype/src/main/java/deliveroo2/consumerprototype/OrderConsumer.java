bdb31f3f9a40a55fa6e5ef372491d5171e16bd4d
Smeef <freek.jansen00@hotmail.com>
2024-05-29 13:04:11 +0200

    YAML update

--- ConsumerPrototype/src/main/java/deliveroo2/consumerprototype/OrderConsumer.java
+++ ConsumerPrototype/src/main/java/deliveroo2/consumerprototype/OrderConsumer.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderConsumer {
 
-    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1", properties = {"spring.json.value.default.type=com.example.consumer.model.Order"})
+    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1")
     public void consume1(Order order, Acknowledgment acknowledgment) {
         System.out.println(String.format("Consumer 1: %s", order.getOrderId()));
         acknowledgment.acknowledge();
     }
 
-    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1", properties = {"spring.json.value.default.type=com.example.consumer.model.Order"})
+    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1")
     public void consume2(Order order, Acknowledgment acknowledgment) {
         System.out.println(String.format("Consumer 2: %s", order.getOrderId()));
         acknowledgment.acknowledge();
     }
 
-    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1", properties = {"spring.json.value.default.type=com.example.consumer.model.Order"})
+    @KafkaListener(topics = "prototype-topic", groupId = "group-id-1")
     public void consume3(Order order, Acknowledgment acknowledgment) {
         System.out.println(String.format("Consumer 3: %s", order.getOrderId()));
         acknowledgment.acknowledge();
